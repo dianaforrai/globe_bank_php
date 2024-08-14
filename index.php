62b1e1e4e16c7b497f725a127483f4cae35b951f
Diana Forrai Susana <97041985+dianaforrai@users.noreply.github.com>
2024-08-14 11:59:01 +0300

    staff

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
-<!doctype html>
+<?php require_once('../../private/initialize.php'); ?>
 
-<html lang="en">
-  <head>
-    <title>Globe Bank</title>
-    <meta charset="utf-8">
-  </head>
+<?php $page_title = 'Staff Menu'; ?>
+<?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-  <body>
+<div id="content">
+</div>
 
-    <h1>Globe Bank: Coming Soon</h1>
-
-  </body>
-</html>
+<?php include(SHARED_PATH . '/staff_footer.php'); ?>
