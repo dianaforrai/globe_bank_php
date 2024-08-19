9f8c4bf3741b7a3d29beca3564f894d3304215ee
Diana Forrai Susana <diana.forrai12@yahoo.com>
2024-08-19 14:11:23 +0300

    changes

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -6,9 +6,9 @@
 
 <html lang="en">
   <head>
-    <title>GBI - <?php echo $page_title; ?></title>
+    <title>GBI - <?php echo h($page_title); ?></title>
     <meta charset="utf-8">
-    <link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>" />
   </head>
 
   <body>
@@ -18,6 +18,6 @@
 
     <navigation>
       <ul>
-        <li><a href="<?php echo 'index.php'; ?>Menu</a></li>
+        <li><a href="<?php echo url_for('/staff/index.php'); ?>">Menu</a></li>
       </ul>
     </navigation>
